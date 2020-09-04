2efdcfcfac73c8b7650593d53c1ea9663994ff48
xiaoYown <qzy09101018@sina.com>
2020-09-04 11:24:33 +0800

    fix: 移除无用文件

--- app_front/apps/public/request/auth.js
+++
@@ -1,9 +0,0 @@
-import fetch from '../utils/fetch'
-
-export function GetVerifyCode (params) {
-  return fetch.get(apis.AuthGetVerifyCode, { params })
-}
-export function AuthRegister (params) {
-  return fetch.post(apis.AuthRegister, params)
-}
-
