20c8b2d543da2a7d67f6eeb7813bcb6151a0e584
xiaoYown <qzy09101018@sina.com>
2020-09-05 14:34:39 +0800

    fix: 分割代码

--- app_front/apps/easy_format/src/App.js
+++ app_front/apps/easy_format/src/App.js
@@ -1,14 +1,19 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import {
   BrowserRouter as Router,
   Switch,
   Route
 } from "react-router-dom";
 
-import PageIndex from './pages/index';
-import PagePugAndHtml from './pages/pugandhtml';
-import PageTextAndBase64 from './pages/textandbase64';
-import PageTinyImage from './pages/tinyimage';
+// import PageIndex from './pages/index';
+// import PagePugAndHtml from './pages/pugandhtml';
+// import PageTextAndBase64 from './pages/textandbase64';
+// import PageTinyImage from './pages/tinyimage';
+
+const PageIndex = React.lazy(() => import(/* webpackChunkName: "index" */ './pages/index'));
+const PagePugAndHtml = React.lazy(() => import(/* webpackChunkName: "pugandhtml" */ './pages/pugandhtml'));
+const PageTextAndBase64 = React.lazy(() => import(/* webpackChunkName: "textandbase64" */ './pages/textandbase64'));
+const PageTinyImage = React.lazy(() => import(/* webpackChunkName: "tinyimage" */ './pages/tinyimage'));
 
 const { BASE_URL } = process.env;
 
@@ -19,26 +24,28 @@ function App() {
         basename={BASE_URL}
       >
         <Switch>
-          <Route
-            exact
-            path="/"
-            component={PageIndex}
-          />
-          <Route
-            exact
-            path={`/pugandhtml`}
-            component={PagePugAndHtml}
-          />
-          <Route
-            exact
-            path={`/textandbase64`}
-            component={PageTextAndBase64}
-          />
-          <Route
-            exact
-            path={`/tinyimage`}
-            component={PageTinyImage}
-          />
+          <Suspense fallback={<div>Loading...</div>}>
+            <Route
+              exact
+              path="/"
+              component={PageIndex}
+            />
+            <Route
+              exact
+              path={`/pugandhtml`}
+              component={PagePugAndHtml}
+            />
+            <Route
+              exact
+              path={`/textandbase64`}
+              component={PageTextAndBase64}
+            />
+            <Route
+              exact
+              path={`/tinyimage`}
+              component={PageTinyImage}
+            />
+          </Suspense>
         </Switch>
       </Router>
     </div>
