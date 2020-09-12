fab9f036c261b2fdd957716c59945f55b8b1daf3
xiaoYown <qzy09101018@sina.com>
2020-09-12 20:38:20 +0800

    fix: 滚动条产生修复

--- app_front/apps/easy_format/src/locale/Provide.jsx
+++ app_front/apps/easy_format/src/locale/Provide.jsx
@@ -1,17 +1,15 @@
 import React from 'react';
-import { Radio, Affix } from 'antd';
+import { Radio } from 'antd';
 import { onChange, lang } from '../i18n';
 
 const lng = lang.get();
 
 function Provide() {
-  return <div>
-    <Affix offsetTop={30} style={{ paddingLeft: '30px' }}>
-      <Radio.Group onChange={onChange} defaultValue={ lng }>
-        <Radio.Button value="en">English</Radio.Button>
-        <Radio.Button value="zh">中文</Radio.Button>
-      </Radio.Group>
-    </Affix>
+  return <div style={{ position: 'fixed', top: '30px', right: '30px' }}>
+    <Radio.Group onChange={onChange} defaultValue={ lng }>
+      <Radio.Button value="en">English</Radio.Button>
+      <Radio.Button value="zh">中文</Radio.Button>
+    </Radio.Group>
   </div>
 }
 
